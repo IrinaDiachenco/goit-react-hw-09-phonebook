5b8630eae13121ca6bf77fc4f8c3819b9bbe2ef1
IrinaDiachenco <65997430+IrinaDiachenco@users.noreply.github.com>
2021-04-02 20:48:02 +0300

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Suspense, lazy } from 'react';
+import React, { Suspense, lazy, useEffect } from 'react';
 import { Switch } from 'react-router-dom';
 import AppBar from '../AppBar/AppBar';
 import Container from '../Container/Container';
@@ -6,7 +6,7 @@ import Container from '../Container/Container';
 // import RegisterView from '../../views/RegisterView/RegisterView';
 // import LoginView from '../../views/LoginView/LoginView';
 // import ContactsView from '../../views/ContactsView/ContactsView';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import authOperations from '../../redux/auth/auth-operations';
 import PrivateRoute from '../Route/PrivatRoute';
 import PublicRoute from '../Route/PublicRoute';
@@ -17,12 +17,21 @@ const RegisterView = lazy(() => import('../../views/RegisterView/RegisterView'))
 const LoginView = lazy(() => import('../../views/LoginView/LoginView'));
 const ContactsView = lazy(() => import('../../views/ContactsView/ContactsView'));
 
-class App extends Component {
-  componentDidMount() {
-    this.props.onGetCurretnUser();
-  }
+// const mapDispatchToProps = {
+//   onGetCurretnUser: authOperations.getCurrentUser,
+// };
 
-  render() {
+// export default connect(null, mapDispatchToProps)(App);
+
+export default function App() {
+  const dispatch = useDispatch();
+  // componentDidMount() {
+  //   this.props.onGetCurretnUser();
+  // }
+
+  useEffect(() => {
+    dispatch(authOperations.getCurrentUser());
+  }, [dispatch]);
     return (
   <Container>
   <AppBar />
@@ -51,11 +60,7 @@ class App extends Component {
 </Container>
 );
 }
-}
 
-const mapDispatchToProps = {
-  onGetCurretnUser: authOperations.getCurrentUser,
-};
 
-export default connect(null, mapDispatchToProps)(App);
+
 
